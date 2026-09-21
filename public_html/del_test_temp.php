87615fc575326f062df221b46fe537e70b6043c2
AionPrimus <aionprimus@users.noreply.github.com>
2026-09-20 21:58:08 -0300

    chore: remove script temporario de limpeza

--- public_html/del_test_temp.php
+++
@@ -1,5 +0,0 @@
-<?php
-if (($_GET['key'] ?? '') !== 'del_test_7781') die();
-require_once __DIR__ . '/gestor/conexao.php';
-$conn->exec("DELETE FROM cadastro WHERE nome_cadastro = 'MARCOS TESTE ONLINE'");
-echo "DELETED_OK";
